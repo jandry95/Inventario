65f409be0537f44acac4e798ed64fa3d03f19d82
KH0RE <cet.quinaluisa@yavirac.edu.ec>
2023-01-22 17:37:35 -0500

    Cedula

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class ClientController extends Controller
 {
     /**
@@ -14,7 +14,10 @@ class ClientController extends Controller
      */
     public function index()
     {
-        $clients = Client::all();
+        $clients = DB::table('clients')
+        ->orderBy('id', 'desc')
+        ->get();
+        // $clients = Client::all();
 
         return response()->json([
             'data' => $clients,
